83a105f8a4247c968964935192be7a674f4aab1e
Ana Mickaelly <ana.aguiar@seno-ti.com.br>
2023-02-18 18:05:18 -0300

    fix: initial commit

--- App.tsx
+++ App.tsx
@@ -3,16 +3,31 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 27 de janeiro de 2022</Text>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  backgroundColor: '#131016',
+  padding: 24,
+
+},
+
+eventName: {
+  color: '#FDFCFE',
+  fontSize: 24,
+  fontWeight: 'bold',
+  marginTop: 48
+},
+
+eventDate: {
+  color:'#6B6B6B',
+  fontSize: 16
+}
+
 });
